a8aeac8d33e4c7ff866f1911d1c105fd99a8da7b
Bess Sadler <bess@curationexperts.com>
2017-08-29 18:34:49 -0400

    Initial passing feature test

--- spec/features/create_etd_spec.rb
+++ spec/features/create_etd_spec.rb
@@ -28,6 +28,20 @@ RSpec.feature 'Create a Etd', js: false do
       # click_button "Create work"
 
       expect(page).to have_content "Add New Etd"
+      visit '/concern/etds/new'
+      fill_in 'Title', with: 'Journey to Skull Island'
+      fill_in 'Creator', with: 'Quest, Jane'
+      fill_in 'Keyword', with: 'Adventure'
+      select('In Copyright', from: 'Rights statement')
+      choose('open')
+      check('agreement')
+      click_on('Files')
+      attach_file('files[]', "#{fixture_path}/fake_text1.pdf")
+      click_on('Save')
+      expect(page).to have_content 'Your files are being processed'
+      expect(page).to have_content 'Journey to Skull Island'
+      expect(page).to have_content 'Quest, Jane'
+      expect(page).to have_content 'Adventure'
     end
   end
 end
