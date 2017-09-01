9f902aa6dfc913684be999a8707ec1dfe5d948be
Bess Sadler <bess@curationexperts.com>
2017-09-01 15:29:36 +0100

    Add custom ETD presenter

---
+++ spec/presenters/etd_presenter_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe EtdPresenter do
+  subject { presenter }
+
+  let(:title) { ['Journey to Skull Island'] }
+  let(:creator) { ['Quest, Jane'] }
+  let(:keyword) { ['Pirates', 'Adventure'] }
+  let(:degree) { ['Master of Pirate Studies'] }
+  let(:visibility) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+  let :etd do
+    Etd.new(
+      title: title,
+      creator: creator,
+      keyword: keyword,
+      degree: degree,
+      visibility: visibility
+    )
+  end
+
+  let(:ability) { Ability.new(user) }
+
+  let(:presenter) do
+    described_class.new(SolrDocument.new(etd.to_solr), nil)
+  end
+
+  # If the fields require no addition logic for display, you can simply delegate
+  # them to the solr document
+  it { is_expected.to delegate_method(:title).to(:solr_document) }
+  it { is_expected.to delegate_method(:degree).to(:solr_document) }
+end
